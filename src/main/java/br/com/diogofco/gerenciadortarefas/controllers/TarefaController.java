e48ec4341a435cd1a8f36afb0bbd47b935609b6e
Diogo Silva <diogo.fransil@gmail.com>
2019-07-17 22:14:47 -0300

    inserindo nova tarefa

--- src/main/java/br/com/diogofco/gerenciadortarefas/controllers/TarefaController.java
+++ src/main/java/br/com/diogofco/gerenciadortarefas/controllers/TarefaController.java
@@ -1,11 +1,19 @@
 package br.com.diogofco.gerenciadortarefas.controllers;
 
+import br.com.diogofco.gerenciadortarefas.models.Tarefa;
 import br.com.diogofco.gerenciadortarefas.repository.RepositoryTarefa;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.Comparator;
+import java.util.Date;
 
 @Controller
 @RequestMapping("/tarefas")
@@ -18,7 +26,39 @@ public class TarefaController {
     public ModelAndView listar() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("tarefas/listar");
-        mv.addObject("tarefas", repositoryTarefa.findAll());
+        mv.addObject("tarefas", repositoryTarefa.findAll(Sort.by(Sort.Direction.DESC, "dataExpiracao")));
+        return mv;
+    }
+
+    @GetMapping("/inserir")
+    public ModelAndView inserir() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("tarefas/inserir");
+        mv.addObject("tarefa", new Tarefa());
+        return mv;
+    }
+
+    @PostMapping("/inserir")
+    public ModelAndView inserir(@Valid Tarefa tarefa, BindingResult result) {
+        ModelAndView mv = new ModelAndView();
+
+        if (tarefa.getDataExpiracao() == null) {
+            result.rejectValue("dataExpiracao", "tarefa.dataExpiracaoInvalida",
+                    "A data de expiração é obrigatória");
+        } else {
+            if (tarefa.getDataExpiracao().before(new Date())) {
+                result.rejectValue("dataExpiracao", "tarefa.dataExpiracaoInvalida",
+                        "A data de expiração não pode ser anterior a data atual");
+            }
+        }
+
+        if (result.hasErrors()) {
+            mv.setViewName("tarefas/inserir");
+            mv.addObject(tarefa);
+        } else {
+            mv.setViewName("redirect:/tarefas/listar");
+            repositoryTarefa.save(tarefa);
+        }
         return mv;
     }
 }
--- src/main/java/br/com/diogofco/gerenciadortarefas/models/Tarefa.java
+++ src/main/java/br/com/diogofco/gerenciadortarefas/models/Tarefa.java
@@ -3,11 +3,11 @@ package br.com.diogofco.gerenciadortarefas.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @AllArgsConstructor
@@ -21,12 +21,16 @@ public class Tarefa {
     private Long id;
 
     @Column(length = 50, nullable = false)
+    @NotNull(message = "O título é obrigatório")
+    @Length(max = 50, min = 3, message = "O título deve conter entre 3 e 50 caracteres")
     private String titulo;
 
     @Column(length = 100, nullable = false)
+    @Length(max = 100, message = "O título deve conter até 100 caracteres")
     private String descricao;
 
     @Column(nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dataExpiracao;
 
     @Column(nullable = false)
